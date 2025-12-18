71bd6c07d123fec5d77f2adf51f0324c260d79db
bogdan_balasoiu <balasoiu.bogdan@gmail.com>
2025-12-18 14:40:04 +0200

    brightness bug fixed

--- Mortal  Vengeance/Assets/Scripts/Menu/BrightnessController.cs
+++ Mortal  Vengeance/Assets/Scripts/Menu/BrightnessController.cs
@@ -3,28 +3,52 @@ using UnityEngine.UI;
 
 public class BrightnessController : MonoBehaviour
 {
+    [Header("Settings")]
+    [SerializeField, Range(0.5f, 0.95f)]
+    private float maxDarkness = 0.85f;
+
     [SerializeField] private Image brightnessOverlay;
     [SerializeField] private Slider brightnessSlider;
 
     private const string PREF_KEY = "Brightness";
 
+    private float targetAlpha;
+    private float currentAlpha;
+
     void Start()
     {
-        float savedValue = PlayerPrefs.GetFloat(PREF_KEY, 0.3f);
+        float savedValue = PlayerPrefs.GetFloat(PREF_KEY, 0.5f);
         brightnessSlider.value = savedValue;
-        ApplyBrightness(savedValue);
 
-        brightnessSlider.onValueChanged.AddListener(ApplyBrightness);
+        currentAlpha = Mathf.Lerp(maxDarkness, 0f, savedValue);
+        targetAlpha = currentAlpha;
+
+        SetAlpha(currentAlpha);
+
+        brightnessSlider.onValueChanged.AddListener(OnSliderChanged);
     }
 
-    void ApplyBrightness(float value)
+    void Update()
     {
-        float alpha = Mathf.Clamp01(value);
+        currentAlpha = Mathf.Lerp(currentAlpha, targetAlpha, Time.unscaledDeltaTime * 8f);
+        SetAlpha(currentAlpha);
+    }
 
-        Color color = brightnessOverlay.color;
-        color.a = alpha;
-        brightnessOverlay.color = color;
-
+    void OnSliderChanged(float value)
+    {
+        targetAlpha = Mathf.Lerp(maxDarkness, 0f, value);
         PlayerPrefs.SetFloat(PREF_KEY, value);
+    }
+
+    void SetAlpha(float alpha)
+    {
+        Color c = brightnessOverlay.color;
+        c.a = alpha;
+        brightnessOverlay.color = c;
+    }
+
+    void OnDestroy()
+    {
+        brightnessSlider.onValueChanged.RemoveListener(OnSliderChanged);
     }
 }
